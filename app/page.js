7346e9857e63256c941f336d1f69306b202d0670
kcdev420 <kevinsmora64@gmail.com>
2026-03-16 17:04:14 -0500

    Add dynamic export to page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import clientPromise from "../lib/mongodb";
 import DashboardClient from "./DashboardClient"; 
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   try {
     const client = await clientPromise;
